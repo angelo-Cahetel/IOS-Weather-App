552ab93dd3f1e2691b8dd60d61cf9dac049f671b
Angelo Mendes <angelo030825@gmail.com>
2024-09-17 12:20:27 -0300

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Ângelo Mendes on 17/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
